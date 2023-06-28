c3e2edbe7fb2cbca0e8cd4f3e072fc256aaf52c9
Chirag Ferwani <chiragferwani@gmail.com>
2023-06-28 16:31:01 +0530

    add project

---
+++ python_projects/U.S.StatesQuizGame/main.py
@@ -0,0 +1,38 @@
+#import modules
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# reading the csv file
+data = pandas.read_csv("50_states.csv")
+# converting the states column to a separate list
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="Enter the states name?").title()
+
+    # check if answer_state is one of the states in the data file
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        # if the answer is right:
+        # create a turtle and write the name on the screen
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)  # or use - state_data.state
