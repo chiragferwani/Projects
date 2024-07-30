666d9c546804e85cb1074e15396d2c1d7a569a08
Chirag Ferwani <chiragferwani@gmail.com>
2024-07-30 15:07:33 +0530

    update

--- python_projects/qrcode.py
+++ python_projects/qrcode.py
@@ -1,4 +1,4 @@
 from PIL import Image
 import qrcode
-img = qrcode.make("https://www.youtube.com/channel/UC3zPELPks5o4Y4_l21VT44Q")
+img = qrcode.make("https://vpnsneakweb.vercel.app/VPNSneak.apk")
 img.save("qrcode.jpg")
